0e49a27ddd8b881f10b3721d7baa3a6735f9a67a
Reyner Veliz <reynerv@yahoo.com>
2022-11-30 23:10:30 -0600

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+
+const App = () => {
+    return(<h1>Hello from App.jsx</h1>);
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+import App from "./App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World</h1>);
+root.render(<App />);
