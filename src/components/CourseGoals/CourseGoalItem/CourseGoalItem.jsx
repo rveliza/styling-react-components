12b997c8f8975218e5577fd3aa5b805b119ff620
Reyner Veliz <reynerv@yahoo.com>
2022-12-01 07:36:06 -0600

    Create deleteHandler for CourseGoalItem

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.jsx
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+const deleteHandler = () => {
+    console.log('This item will be deleted');
+}
+
 const CourseGoalItem = () => {
     return (
-        <li className="goal-item">
+        <li className="goal-item" onClick={deleteHandler}>
             This is a list item!
         </li>
     );
