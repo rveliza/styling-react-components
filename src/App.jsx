1ce4ea3b33b0feec9d899a72b965608bd866df82
Reyner Veliz <reynerv@yahoo.com>
2022-11-30 23:29:26 -0600

    Add decision making to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
 
 const App = () => {
-    return(<h1>Hello from App.jsx</h1>);
+    const [courseGoals, setCourseGoals] = useState([
+        { text: 'Do all exercises!', id: 'g1' },
+        { text: 'Finish the course!', id: 'g2' }
+    ]);
+
+    let content = (
+        <p style={{ textAling: 'center' }}>No goals found. Maybe add one?</p>
+    );
+
+    if (courseGoals.length > 0) {
+        content = (
+            <p>There are many goals!</p>
+        );
+    }
+
+    return(
+        <div>
+            <section id="goal-form">
+                <h3>Coarse input goes here</h3>
+            </section>
+            <section id="goals">
+                {content}
+            </section>
+        </div>
+    );
 }
 
 
